d33613c8bc1aeec5676070cad33534a43fc7aaa3
Andrew Souhrada <andrewsouhrada@yahoo.com>
2016-04-14 14:14:54 -0500

    Selectable Months
    There is now a JComboBox that allows the user to select the month which
    sets the amount of days shown in the calendar grid.

--- Homework Planner/HomeworkPlanner.java
+++ Homework Planner/HomeworkPlanner.java
@@ -1,13 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
-import javax.swing.JPanel;
 import java.awt.GridLayout;
 import java.awt.Container;
 import javax.swing.JButton;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerListModel;
-import javax.swing.SpinnerModel;
+import javax.swing.JComboBox;
 import java.text.DateFormatSymbols;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 
 /**
@@ -20,10 +19,12 @@ public class HomeworkPlanner extends JFrame
 {
     private static final int FRAME_WIDTH = 800;
     private static final int FRAME_HEIGHT = 800;
-    private Container pane;
+    private Container pane = getContentPane();
     private JButton day;
-    private JSpinner month;
-    private DateFormatSymbols symbols;
+    private DateFormatSymbols symbols = new DateFormatSymbols();
+    private String months[] = symbols.getMonths();
+    private JComboBox<String> monthList;
+    private ArrayList<JButton> grid = new ArrayList<JButton>();
     
     /**
      * Constructor for objects of class HomeworkPlanner
@@ -32,8 +33,13 @@ public class HomeworkPlanner extends JFrame
     {
         this.setSize( FRAME_WIDTH, FRAME_HEIGHT );
         
-        pane = getContentPane();
-        createCalendar( 31 );
+        monthList = new JComboBox<String>( months );
+        this.add( monthList );
+        
+        ActionListener monl = new ComboBoxListener();
+        this.monthList.addActionListener( monl );
+        
+        createCalendar();
         
         this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         this.pack();
@@ -57,19 +63,90 @@ public class HomeworkPlanner extends JFrame
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public void createCalendar( int numdays )
+    public void createCalendar()
     {
-        symbols = new DateFormatSymbols();
-        String months[] = symbols.getShortMonths();
-        SpinnerModel model1 = new SpinnerListModel( months );
-        JSpinner spinner1 = new JSpinner( model1 );
-        this.add( spinner1 );
-        
-        pane.setLayout( new GridLayout( 5, 7 ) );
-        for( int i = 0; i < numdays; i++ )
+        pane.setLayout( new GridLayout( 7, 4 ) );
+        for( int i = 0; i < 49; i++ )
         {
-            day = new JButton( Integer.toString( i + 1 ) );
+            day = new JButton();
+            grid.add( day );
             pane.add( day );
+        }
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y
+     */
+    public void updateCalendar( int days )
+    {
+        for( int i = 0; i < grid.size(); i++ )
+        {
+            grid.get( i ).setText( "" );
+        }
+        for( int i = 0; i < days; i++ )
+        {
+            grid.get( i ).setText( Integer.toString( i + 1 ) );
+        }
+    }
+
+    
+    
+    class ComboBoxListener implements ActionListener
+    {
+        public void actionPerformed( ActionEvent event )
+        {
+            JComboBox cb = (JComboBox)event.getSource();
+            if( cb.getSelectedItem() == "January"  )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "February" )
+            {
+                updateCalendar( 29 );
+            }
+            else if( cb.getSelectedItem() == "March" )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "April" )
+            {
+                updateCalendar( 30 );
+            }
+            else if( cb.getSelectedItem() == "May" )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "June" )
+            {
+                updateCalendar( 30 );
+            }
+            else if( cb.getSelectedItem() == "July" )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "August" )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "September" )
+            {
+                updateCalendar( 30 );
+            }
+            else if( cb.getSelectedItem() == "October" )
+            {
+                updateCalendar( 31 );
+            }
+            else if( cb.getSelectedItem() == "November" )
+            {
+                updateCalendar( 30 );
+            }
+            else if( cb.getSelectedItem() == "December" )
+            {
+                updateCalendar( 31 );
+            }
         }
     }
 }
