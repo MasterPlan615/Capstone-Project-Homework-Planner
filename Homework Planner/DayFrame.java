acebf1e23f6fd3fc979748e55fbe0dd74587a915
Andrew Souhrada <andrewsouhrada@yahoo.com>
2016-04-25 14:11:58 -0500

    Exceptions and TextArea
    Add try and catch statements for exceptions and made it so the text
    field text would be displayed in the text area.

--- Homework Planner/DayFrame.java
+++ Homework Planner/DayFrame.java
@@ -2,6 +2,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
@@ -15,8 +23,13 @@ public class DayFrame extends JFrame
     private static final int FRAME_WIDTH = 400;
     private static final int FRAME_HEIGHT = 400;
     private JLabel numday;
-    private JTextField notes;
+    private JTextField creno;
     private FlowLayout lay = new FlowLayout();
+    private JTextArea notes;
+    private JScrollPane sp;
+    private File notesFile;
+    private Scanner in;
+    private ActionListener notl = new NoteListener();
     
     
     /**
@@ -27,11 +40,37 @@ public class DayFrame extends JFrame
         this.setSize( FRAME_WIDTH, FRAME_HEIGHT );
         this.setLayout( lay );
         
+        try
+        {
+            notesFile = new File( "notesFile.txt" );
+            try
+            {
+                notesFile.createNewFile();
+            }
+            catch ( IOException a )
+            {
+                System.err.println( "IOException: " + a.getMessage() );
+            }
+            in = new Scanner( notesFile );
+        }
+        catch( FileNotFoundException e )
+        {
+            System.err.println( "FileNotFoundException: " + e.getMessage() );
+        }
+        
         this.numday = new JLabel( " " );
         this.add( this.numday );
         
-        this.notes = new JTextField( 25 );
+        this.creno = new JTextField( 25 );
+        this.add( this.creno );
+        
+        this.notes = new JTextArea( 15, 20 );
+        this.sp = new JScrollPane( notes );
+        notes.setEditable( false );
         this.add( this.notes );
+        
+        ActionListener notl = new NoteListener();
+        this.creno.addActionListener( notl );
         
         this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         //this.pack();
@@ -47,5 +86,16 @@ public class DayFrame extends JFrame
     public void updateDay( String month, String day )
     {
         this.numday.setText( month + " " + day );
+    }
+    
+    
+    class NoteListener implements ActionListener
+    {
+        public void actionPerformed( ActionEvent event )
+        {
+            JTextField newf = (JTextField)event.getSource();
+            String usernotes = newf.getText();
+            notes.setText( usernotes );
+        }
     }
 }
